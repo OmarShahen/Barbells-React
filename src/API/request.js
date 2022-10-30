762e64b37c6e0c4f04d764029caaf119a1d92802
Omar Reda <omarredaelsayedmohamed@gmail.com>
2022-10-30 21:33:15 +0200

    added production URL

--- src/API/request.js
+++ src/API/request.js
@@ -4,5 +4,5 @@ const DEV_URL = 'http://localhost:5000/api/v1'
 const PROD_URL = 'http://159.223.172.150/api/v1'
 
 export const serverRequest = axios.create({
-    baseURL: DEV_URL
+    baseURL: PROD_URL
 })
